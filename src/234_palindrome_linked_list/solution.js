ee94a14f3a22d3df7e32891a70d9778a01b84cea
Motoki Naito <motoki.developer@gmail.com>
2020-01-18 22:11:05 +0900

    Solve Palindrome Linked List

---
+++ src/234_palindrome_linked_list/solution.js
@@ -0,0 +1,40 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+const isPalindrome = head => {
+  if (head == null || head.next == null) return true;
+  let slow = head;
+  let fast = head;
+  while (fast.next != null && fast.next.next != null) {
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+  slow.next = reverseList(slow.next);
+  slow = slow.next;
+  while (slow != null) {
+    if (head.val != slow.val) return false;
+    head = head.next;
+    slow = slow.next;
+  }
+  return true;
+};
+
+const reverseList = head => {
+  let pre = null;
+  let next = null;
+  while (head != null) {
+    next = head.next;
+    head.next = pre;
+    pre = head;
+    head = next;
+  }
+  return pre;
+};
