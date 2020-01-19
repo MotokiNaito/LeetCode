bbe7b413cc807e8fa2a1d09d81271757f78354ab
Motoki Naito <motoki.developer@gmail.com>
2020-01-19 17:09:27 +0900

    Solve Linked List Cycle

---
+++ src/141_linked_list_cycle/solution.js
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+const hasCycle = (head) => {
+  if (!head) {
+    return false;
+  }
+  let hash = {};
+  while (head) {
+    if (hash[head.val]) {
+      for (let i = 0; i < hash[head.val].length; i++) {
+        if (head === hash[head.val][i]) {
+          return true;
+        }
+      }
+      hash[head.val].push(head);
+    } else {
+      hash[head.val] = [head];
+    }
+    head = head.next;
+  }
+  return false;
+};
