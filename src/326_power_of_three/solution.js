cb048314d318bb706e3e144f8d486d8e65325ca1
Motoki Naito <motoki.developer@gmail.com>
2020-01-27 21:19:42 +0900

    Solve Power of Three

---
+++ src/326_power_of_three/solution.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+const isPowerOfThree = (n) => {
+  if (n <= 0) {
+    return false;
+  }
+  if (n === 1) {
+    return true;
+  }
+  while (n % 3 === 0) {
+    n /= 3;
+  }
+  return n === 1;
+};
